f2a3853ea1ff4d6c06bba142f9d5160e474446a9
Maria6112 <127786529+Maria6112@users.noreply.github.com>
2025-09-22 16:09:38 +0300

    logo changed

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -140,7 +140,7 @@ const Navbar = () => {
         role="button"
         tabIndex={0}
       >
-        <img src="images/N.png" alt="logo" className="logo-img" />
+        <img src="images/LibaFilms1.svg" alt="logo" className="logo-img" />
       </div>
       <div className="burger" ref={burgerRef}>
         <button
--- src/components/VideoGallery.js
+++ src/components/VideoGallery.js
@@ -28,24 +28,30 @@ const translations = {
 const videoSources = [
   {
     id: "1",
+    type: "dropbox",
     videoUrl:
       "https://www.dropbox.com/scl/fi/h7z88h2zrgz203bsergm2/N-D.mp4?rlkey=mwpb8p3pi9l4711x8hoxfvdfp&raw=1",
+
     // thumbnail: "/images/dvir&noa.png",
     // title: "Dvir & Noa",
   },
   {
     id: "2",
+    type: "vimeo",
     videoUrl:
-      "https://www.dropbox.com/scl/fi/yu7ksilmwacgcdtnljn6b/04.12.24.mp4?rlkey=rz0813nocvcgnb1mi2t0sslmf&e=1&st=kwi2wc5j&raw=1",
+      // "https://www.dropbox.com/scl/fi/yu7ksilmwacgcdtnljn6b/04.12.24.mp4?rlkey=rz0813nocvcgnb1mi2t0sslmf&e=1&st=kwi2wc5j&raw=1",
+      "https://player.vimeo.com/video/1118986580?h=3503314126",
   },
   {
     id: "3",
+    type: "dropbox",
     videoUrl:
       "https://www.dropbox.com/scl/fi/2rundd75ngvguz8vtelez/.mp4?rlkey=txkhhsa2zqjcmecdtj4oio492&st=y7u9umov&raw=1",
     thumbnail: "/images/roni&oz.jpg",
   },
   {
     id: "4",
+    type: "dropbox",
     videoUrl:
       "https://www.dropbox.com/scl/fi/t5oo7ql576x8zsiuclv05/.mp4?rlkey=6flplhuxubizo4sfmo6ho3ha5&st=n5nto0qp&raw=1",
   },
@@ -67,15 +73,35 @@ const VideoGallery = () => {
       {videoSources.map((video, index) => (
         <div className="video-card" key={video.id}>
           <div className="video-wrapper">
-            <video
-              src={video.videoUrl}
-              poster={video.thumbnail}
-              controls
-              controlsList="nodownload"
-              onContextMenu={(e) => e.preventDefault()}
-              preload="metadata"
-              className="video-full"
-            ></video>
+            {video.type === "dropbox" ? (
+              <video
+                src={video.videoUrl}
+                poster={video.thumbnail}
+                controls
+                controlsList="nodownload"
+                onContextMenu={(e) => e.preventDefault()}
+                preload="metadata"
+                className="video-full"
+                // onClick={(e) => {
+                //   if (e.target.paused) {
+                //     e.target.play();
+                //   } else {
+                //     e.target.pause();
+                //   }
+                // }}
+              ></video>
+            ) : (
+              <iframe
+                src={video.videoUrl}
+                width="100%"
+                height="360"
+                frameBorder="0"
+                allow="autoplay; fullscreen; picture-in-picture"
+                allowFullScreen
+                className="video-full"
+                title={`vimeo-video-${video.id}`}
+              ></iframe>
+            )}
           </div>
 
           <div className="video-title-box">
